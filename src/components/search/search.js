990d5071ec9ed7e06b4ec5d6d03ba9d6c2b33213
ClaudioF9 <claudioferreira0284@gmail.com>
2023-03-24 20:53:08 +0000

    sorting errors on netlify

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -2,7 +2,6 @@
 import SearchBar from "../searchbar/searchbar";
 import "./search.css"
 import ReturnDate from "../returndate/returndate";
-import { DepartureBoard } from "@mui/icons-material";
 import DepartureDate from "../departuredate/departuredate";
 
 function Search() {
